051fa6214f90dba7adaa2a5cf85f45661ab31054
kgv <kgv@users.noreply.github.com>
2026-05-20 12:18:22 +0300

    2

--- src/const.rs
+++ src/const.rs
@@ -1,13 +1,13 @@
 pub const FAE: &str = "FAE";
 pub const PREFIX: &str = FAE;
 
-// pub const PRIMITIVE: &str = "Primitive";
-// pub const RATIO: &str = "Ratio";
-// pub const SUM: &str = "Sum";
+pub const PRIMITIVE: &str = "Primitive";
+pub const RATIO: &str = "Ratio";
+pub const SUM: &str = "Sum";
 
-// pub const BIODIESEL: &str = "Biodiesel";
-// pub const METABOLIC: &str = "Metabolic";
-// pub const NUTRITIONAL: &str = "Nutritional";
+pub const BIODIESEL: &str = "Biodiesel";
+pub const METABOLIC: &str = "Metabolic";
+pub const NUTRITIONAL: &str = "Nutritional";
 
 pub mod sum {
     pub const SUMS: [&str; 17] = [
